191f6cd046ec1d9e43e2290bdb15e5aa005e6ec2
selamawit abdo <selamawitabdo@selamawits-MacBook-Pro.local>
2022-04-15 16:21:22 -0600

    Updated README.md file

--- traffic-violation-service/src/main/java/com/nvs/trafficviolation/service/DefaultWebSecurityConfig.java
+++ traffic-violation-service/src/main/java/com/nvs/trafficviolation/service/DefaultWebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,14 +24,14 @@ public class DefaultWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http
-        //         .cors().and()
-        //         .csrf().disable()
-        //         .authorizeRequests()
-        //         .antMatchers("/rest/*").authenticated().and()
-        //         .httpBasic().and()
-        //         .headers().frameOptions().disable();
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/rest/*").authenticated().and()
+                .httpBasic().and()
+                .headers().frameOptions().disable();
+        // http.authorizeRequests().antMatchers("/").permitAll();
         
     }
 
